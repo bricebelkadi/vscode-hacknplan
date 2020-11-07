039a2a110391738635343d3bd167c0a5f6e2cff6
Brice Belkadi <brice.belkadi@gmail.com>
2020-11-07 13:49:31 +0100

    milestone tree items working

--- src/models/board.model.ts
+++ src/models/board.model.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import { threadId } from "worker_threads";
 import { ModelType } from "./core.model";
 
 export class Board {
@@ -10,6 +11,7 @@ export class Board {
 
   projectId!: number;
   boardId!: number;
+  milestoneId?: number;
   name!: string;
   description!: string;
   dueDate!: Date;
@@ -24,6 +26,34 @@ export class BoardTreeItem extends vscode.TreeItem {
     public readonly type : ModelType,
     public readonly boardId : number,
     public readonly projectId: number
+  ) {
+    super(label, collapsibleState);
+  }
+}
+
+export class MileStone {
+  constructor(obj: any) {
+    Object.assign(this, obj);
+    this.creationDate = new Date(obj.creationDate);
+    this.boards = obj.boards.map((x: any) => new Board(x));
+
+  }
+  projectId!: number;
+  milestoneId!: number;
+  name!: string;
+  creator!: any;
+  creationDate!: Date;
+  boards!: Board[];
+}
+
+export class MilestoneTreeItem extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public readonly type: ModelType,
+    public readonly milestoneId: number,
+    public readonly projectId: number,
+    
   ) {
     super(label, collapsibleState);
   }

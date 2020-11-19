bba2d4fc957f253102144b5ba030b1ffd4de32ce
Brice Belkadi <brice.belkadi@gmail.com>
2020-11-19 21:49:53 +0100

    reboot vscode

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import Axios, { AxiosRequestConfig } from "axios";
 import * as vscode from "vscode";
-import { Task } from "./models/task.model";
-import TaskService from "./services/task.service";
+import { MessageService } from "./services/message.service";
 import { UserService } from "./services/user.service";
 import { MainTreeContainer } from "./views/tree/main.tree";
-import ShowTask from "./views/webview/task.webview";
-import * as path from "path";
-import StorageService from "./services/storage.service";
-import { MessageService } from "./services/message.service";
-import { MainWebview } from "./views/webview/main.webview";
+import { TaskWebview } from "./views/webview/main.webview";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,11 +13,7 @@ export async function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "hacknplan" is now active!');
-
-  // Store extension Path
-
-  StorageService.storeExtensionPath(context.extensionPath);
-
+  
   const defineApiCommand = vscode.commands.registerCommand(
     'hacknplan.defineAPIKey',
     MessageService.defineAPIKeyMessage
@@ -32,12 +23,13 @@ export async function activate(context: vscode.ExtensionContext) {
 
   // Set Api Key through Axios interceptor
   const apiKey = await vscode.workspace.getConfiguration("hacknplan").get("apiKey");
-  console.log(apiKey)
+  console.log(apiKey);                                                           
   if (apiKey) {
     Axios.interceptors.request.use((config: AxiosRequestConfig) => {
       config.headers.Authorization = `ApiKey ${apiKey}`;
       return config;
     });
+    MainTreeContainer.projectTreeProvider.refresh();
   }
 
   await UserService.getAndStoreMe();
@@ -46,9 +38,9 @@ export async function activate(context: vscode.ExtensionContext) {
 
   // const mainTree = new MainTreeContainer();
 
-  const showTaskDetailCommand = MainWebview.showTaskCommand;
+  const taskWebview = new TaskWebview(context.extensionPath);
 
-  context.subscriptions.push(showTaskDetailCommand);
+  context.subscriptions.push(taskWebview.showTaskCommand);
 
 
     // const showTaskDetailCommand = vscode.commands.registerCommand(
